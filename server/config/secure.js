e612819a22a2691572c976b6b470e346c06bc645
ming_zhe3 <shixi_mingzhe3@staff.sina.com>
2022-03-07 17:58:21 +0800

    withoout server

--- server/config/secure.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-module.exports = {
-  qiniuConfig: {
-    domain: 'http://qiniu.hackslog.cn',
-    AK: 'JQ4UeoKqljOt5Q70WBDSNfla8nLZL-lgd2K8TBhN',
-    SK: 'ibu14yew4g6CTNAFTis-Gqwy5dvEPPdg4xOnvIJN',
-    bucket: 'imgxioashi',
-  },
-};
